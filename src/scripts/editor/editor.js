c9b678be4e36cbb17aafdd31ba428f0c4695d033
Manlio Barajas <manlito@gmail.com>
2022-03-01 21:54:43 -0800

    Minor editor UI fixes

--- src/scripts/editor/editor.js
+++ src/scripts/editor/editor.js
@@ -1712,7 +1712,7 @@ function surveyEditor(events) {
 			testMode: true,
 			surveyId: 'surveyPreview' + utils.randomId(),
 			containerId: contentPlaceholder.attr('id'),
-			height: 350,
+			height: 290,
 			controlClass: 'form-control',
 			data: editor.getSurvey(),
 			animation: {
